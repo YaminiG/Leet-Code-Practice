5cb8eb25cad19797254d2b7628cd40befba3d6a5
YaminiG <35190696+alicecrush@users.noreply.github.com>
2018-09-02 17:59:49 -0400

    code to check whether two trees are identical in terms of structure and node data

---
+++ sameTree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        String postOrder = "";
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null){
+            return false;
+        }
+        String inOrderForP = inOrder(p);
+        System.out.println(inOrderForP);
+        String inOrderForQ = inOrder(q);
+        System.out.println(inOrderForQ);
+        if(inOrderForP.equals(inOrderForQ)){
+            return true;
+        }
+        System.out.println(inOrderForP);
+        return false;
+        
+    }
+    
+    public String inOrder(TreeNode root){
+        if(root==null){
+            return "here";
+        }
+        String inOrderString ="" + root.val + "" + inOrder(root.right) + "" + inOrder(root.left) ; 
+        return inOrderString;
+    }
+}
